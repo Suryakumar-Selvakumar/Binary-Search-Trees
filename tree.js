38c7521c661906083005e1f24486f70666926efa
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-11 08:48:07 -0600

    Create insert(value) function that inserts a value into binary

--- tree.js
+++ tree.js
@@ -11,6 +11,24 @@ export class Tree {
     array.sort((a, b) => a - b);
     this.root = sortedArrtoBST(array, start, end);
   }
+
+  insert(value) {
+    let curNode = this.root;
+
+    while (curNode.left !== null || curNode.right !== null) {
+      if (value < curNode.data) {
+        curNode = curNode.left;
+      } else if (value >= curNode.data) {
+        curNode = curNode.right;
+      }
+    }
+
+    if (value < curNode.data) {
+      curNode.left = new Node(value);
+    } else if (value >= curNode.data) {
+      curNode.right = new Node(value);
+    }
+  }
 }
 
 function sortedArrtoBST(array, start, end) {
@@ -40,7 +58,10 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const tree = new Tree();
-tree.buildTree(arr);
-console.log(prettyPrint(tree.root));
+// const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// const tree = new Tree();
+// tree.buildTree(arr);
+// console.log(tree.root);
+// console.log(prettyPrint(tree.root));
+// tree.insert(20);
+// console.log(prettyPrint(tree.root));
